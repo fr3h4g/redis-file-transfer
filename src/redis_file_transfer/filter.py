0915e98b1adaa2460a65ae14439dcab1543b16a0
Fredrik Haglund <fredrik@petitcircuitlab.com>
2022-12-01 19:55:20 +0100

    fix: change type

--- src/redis_file_transfer/filter.py
+++ src/redis_file_transfer/filter.py
@@ -1,8 +1,7 @@
 import re
-from re import Pattern
 
 
-def filename_filter(filename: str, include: Pattern[str], exclude: Pattern[str]):
+def filename_filter(filename: str, include: str, exclude: str):
     if re.match(include, filename) and not re.match(exclude, filename):
         return True
     return False
